a6a0610f6eba7fa2bad41a538aacd860b40ec288
Nosieek <mateusz.nosko13@gmail.com>
2022-05-16 19:17:13 +0200

    Update models, added categories field and model

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField('Category', related_name='posts')
 
     def __str__(self):
-        return f'{self.title}, author: {self.author}'
+        return f'Title: {self.title}\n author: {self.author}'
+
+    def get_absolute_url(self):
+        return reverse('blog-home')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Category: {self.name}"
