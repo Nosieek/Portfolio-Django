83ffe93480fe61973e10cc36d6adc90680cfd8d5
Nosieek <mateusz.nosko13@gmail.com>
2022-05-16 19:57:39 +0200

    User management applications were created

---
+++ users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+class UserRegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+class UserLoginView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/login.html'
+    success_url = reverse_lazy('blog-home')
