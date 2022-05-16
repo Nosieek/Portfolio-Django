b55ca00cea2427e6e6f929754a7d987373c3ff3e
Nosieek <mateusz.nosko13@gmail.com>
2022-05-16 19:23:13 +0200

    New views in home page

--- home_page/views.py
+++ home_page/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .models import Project
+from django.views.generic import ListView, DetailView
 
-def project_index(request):
-    projects = Project.objects.all()
-    context = {
-        'projects': projects
-    }
-    return render(request, 'project_index.html', context)
+# def project_index(request):
+#     projects = Project.objects.all()
+#     context = {
+#         'projects': projects
+#     }
+#     return render(request, 'project_home.html', context)
+
+class project_index(ListView):
+    model = Project
+    context_object_name = 'projects'
+    template_name = 'project_home.html'
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
@@ -14,3 +20,10 @@ def project_detail(request, pk):
         'project': project
     }
     return render(request, 'project_detail.html', context)
+
+# class project_detail(DetailView):
+#     model = Project
+#     pk_url_kwarg = 'pk'
+#     context_object_name = 'projects'
+#     template_name = 'project_detail.html'
+
