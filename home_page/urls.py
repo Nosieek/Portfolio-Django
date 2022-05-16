b7d95562b29cf0a203aa7dddb40b881204fc0331
Nosieek <mateusz.nosko13@gmail.com>
2022-05-16 19:21:18 +0200

    Refactor path

--- home_page/urls.py
+++ home_page/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from . import views
+from .views import project_index
 
 urlpatterns = [
-    path("", views.project_index, name="project_index"),
+    path("", project_index.as_view(), name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
