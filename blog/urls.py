09d5fa00539cffeacaa50ea0dfb25172c8d6204b
Nosieek <mateusz.nosko13@gmail.com>
2022-05-16 19:17:38 +0200

    Added new paths website

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-# from . import views
-from .views import BlogView, BlogDetailView
+from . import views
+from .views import BlogView, BlogDetailView, AddPostView,UpdatePostView,DeletePostView
 
 urlpatterns = [
     path('', BlogView.as_view(), name="blog-home"),
+    path('add_post/', AddPostView.as_view(), name='add-post'),
+    path('<int:pk>/delete_post/', DeletePostView.as_view(), name='delete-post'),
+    path('edit/<int:pk>', UpdatePostView.as_view(), name='update-post'),
     path("<int:pk>/", BlogDetailView.as_view(), name='blog-detail'),
+    path('<category>/', views.blog_category, name='blog-category'),
+
 ]
