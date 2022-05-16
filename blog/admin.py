5f57e551c175001a627175934ce79a9bfafd079d
Nosieek <mateusz.nosko13@gmail.com>
2022-05-16 19:14:27 +0200

    Update admin
    Register category in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass
