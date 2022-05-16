a0182c70d9ba78125ea230d53e357b49ce381582
Nosieek <mateusz.nosko13@gmail.com>
2022-05-16 20:31:57 +0200

    Was added category path to adding

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import BlogView, BlogDetailView, AddPostView,UpdatePostView,DeletePostView
+from .views import BlogView, BlogDetailView, AddPostView,UpdatePostView,DeletePostView, AddCategoryView
 
 urlpatterns = [
     path('', BlogView.as_view(), name="blog-home"),
     path('add_post/', AddPostView.as_view(), name='add-post'),
+    path('add_category/', AddCategoryView.as_view(), name='add-category'),
     path('<int:pk>/delete_post/', DeletePostView.as_view(), name='delete-post'),
     path('edit/<int:pk>', UpdatePostView.as_view(), name='update-post'),
     path("<int:pk>/", BlogDetailView.as_view(), name='blog-detail'),
