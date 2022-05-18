daa02891ab6f12b97b54f8e77d71fd81d3744cdf
Nosieek <mateusz.nosko13@gmail.com>
2022-05-18 22:29:46 +0200

    fixed issue with adding posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import Post
+from .models import Post,Category
+
+choises = Category.objects.all().values_list('name','name')
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -8,7 +10,7 @@ class PostForm(forms.ModelForm):
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Put your title of post!'}),
-            'categories': forms.Select(attrs={'class': 'form-control'}),
+            'categories': forms.SelectMultiple(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control','placeholder': 'Put your body of post!'}),
         }
